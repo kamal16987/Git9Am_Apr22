281636e9881c1ff23fd5941f18da079696ff3fcf
Alexis Prabahaaran <alexisprabaharan@gmail.com>
2019-04-26 06:06:39 +0530

    Repository

---
+++ src/com/test/pacl/Alex.java
@@ -0,0 +1,16 @@
+package com.test.pacl;
+
+public class Alex {
+public static void main(String[] args) {
+	int a=1;
+	int b=2;
+	int c;
+	
+	for (int i=1; i<=7; i++){
+		System.out.println(a+"");
+	c=a+b;
+	a=b;
+	b=c;
+	}
+}
+}
