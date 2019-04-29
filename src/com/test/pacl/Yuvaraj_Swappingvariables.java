577fd51d811f8cb60b0663ff541132dd2c308a57
Yuvarajarun <yuvarajarun1@gmail.com>
2019-04-29 19:37:31 +0530

    Yuvaraj-S

---
+++ src/com/test/pacl/Yuvaraj_Swappingvariables.java
@@ -0,0 +1,15 @@
+package com.test.pacl;
+
+public class Yuvaraj_Swappingvariables {
+    public static void main(String[] args) {
+		
+    	int a=3;
+    	int b=4;
+    	
+    	a=a+b;
+    	b=a-b;
+    	a=a-b;
+    	
+    	System.out.println("a is "+a+"b is "+b);
+	}
+}
---
+++ src/com/test/pacl/Yuvaraj_methodoverloading.java
@@ -0,0 +1,16 @@
+package com.test.pacl;
+
+public class Yuvaraj_methodoverloading {
+public void pubic(int b) {
+	System.out.println(b);
+}
+    public void Num(String a ) {
+	System.out.println(a);
+}
+    public void main() {
+    	Yuvaraj_methodoverloading O=new Yuvaraj_methodoverloading();
+    	O.Num("yuvi");
+    	O.pubic(54321);
+	}
+    
+}
