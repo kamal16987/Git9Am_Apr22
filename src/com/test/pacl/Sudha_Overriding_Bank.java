b110cddd97acc9eeb66003a9e84b69e35846b1b9
sudharsana0514 <sudharsana.sona@gmail.com>
2019-04-29 20:02:21 +0530

    Overriding code by Sudharsana

---
+++ src/com/test/pacl/Sudha_Overriding_Bank.java
@@ -0,0 +1,28 @@
+package com.test.pacl;
+
+class Bank{  
+int getRateOfInterest(){return 0;}  
+}  
+ 
+class SBI extends Bank{  
+int getRateOfInterest(){return 8;}  
+}  
+  
+class ICICI extends Bank{  
+int getRateOfInterest(){return 7;}  
+}  
+class AXIS extends Bank{  
+int getRateOfInterest(){return 9;}  
+}  
+//Test class to create objects and call the methods  
+class Sudha_Overriding_Bank{  
+public static void main(String args[]){  
+SBI s=new SBI();  
+ICICI i=new ICICI();  
+AXIS a=new AXIS();  
+System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());  
+System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());  
+System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());  
+}  
+}  
+
