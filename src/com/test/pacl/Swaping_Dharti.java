17d855878dfdd0dd4844cf68bb5a749cdfc79e7d
Dharti19 <dhartirai@gmail.com>
2019-04-30 11:45:57 +0530

    Traingle and method overload added by Dharti

--- src/com/test/pacl/Swaping_Dharti.java
+++ src/com/test/pacl/Swaping_Dharti.java
@@ -12,11 +12,10 @@ public class Swaping_Dharti {
 		System.out.println("Enter the number for swapping:");
 		System.out.println("Entter first number");
 		int a=sc.nextInt();
-		//System.out.println("First Number befor swapping is a: "+a);
+		
 		System.out.println("Enter Second number");
 		int b = sc.nextInt();
-		//System.out.println("Second Number befor swapping is b: "+b);
-		
+	
 		a=a+b;
 		b=a-b;
 		a=a-b;
---
+++ src/com/test/pacl/Triangle_Dharti.java
@@ -0,0 +1,23 @@
+package com.test.pacl;
+
+public class Triangle_Dharti {
+
+	
+
+		public static void main(String[] args) {
+			System.out.println("Program for printing number patter");
+			
+			for (int i =5; i >=1; i--) {
+				for (int j = 1; j <=i; j++) {
+					System.out.print(j);
+				}
+				System.out.println("");
+			}
+		}
+		
+		
+	}
+	
+	
+	
+
