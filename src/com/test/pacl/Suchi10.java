fcaeeedd43d3c9133a46c17b346230fd109b712f
sucharitha28589 <suchi.yudy@gmail.com>
2019-05-08 20:10:49 +0530

    added class

--- src/com/test/pacl/Suchi10.java
+++ src/com/test/pacl/Suchi10.java
@@ -1,5 +1,23 @@
 package com.test.pacl;
 
-public class Suchi10 {
+import java.util.Scanner;
 
-}
+public class Suchi10 {
+	public static void main(String[] args) {
+	String original, reverse="";
+	System.out.println("Enter a String");
+	Scanner s=new Scanner(System.in);
+	original=s.nextLine();
+	int length=original.length(); 
+	
+	for (int i = length-1; i >=0; i--) {
+		reverse=reverse+original.charAt(i);
+		
+	}if (original.equals(reverse)) {
+		System.out.println("palindrome");
+		
+	}else {
+		System.out.println("not a palindrome");
+	}
+
+}}
